f38013137460cf42555cdd16b468f2273b01e728
MehrabRashid <charlesdicken112@gmail.com>
2026-08-25 20:32:43 +0600

    Update proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -124,6 +124,9 @@ allocproc(void)
 found:
   p->pid = allocpid();
   p->state = USED;
+  p->rnd=0;
+  p->tkt=10;
+
 
   // Allocate a trapframe page.
   if ((p->trapframe = (struct trapframe *)kalloc()) == 0) {
